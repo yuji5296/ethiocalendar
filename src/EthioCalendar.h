49b658504c21f1800605c9391e106d07fc58a612
yuji5296@gmail.com <yuji5296@gmail.com@cabc5e30-087a-11de-9036-69e38a880166>
2012-06-27 15:39:00 +0000

    Ver.1.6.3
    Delete JICA logo

--- src/EthioCalendar.h
+++ src/EthioCalendar.h
@@ -7,7 +7,7 @@
 #ifndef ethiocalendar_h
 #define ethiocalendar_h
 #define ETHIOCALENDAR_URI	  "http://code.google.com/p/ethiocalendar/"
-#define ETHIOCALENDAR_VER	"Ver.1.6.2"
+#define ETHIOCALENDAR_VER	"Ver.1.6.3"
 #define ETHIOCALENDAR_NAME	 "Ethiopian Calendar"
 #define ETHIOCALENDAR_TITLE	  ETHIOCALENDAR_NAME " " ETHIOCALENDAR_VER
 #endif
